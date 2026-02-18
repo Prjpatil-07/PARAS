c55715eb070f485213586d9fbae0b4869baef384
SANE_DEVRAJ\PARAS <parasrjpatil111@gmail.com>
2026-02-18 11:15:53 +0530

    For loop

--- trial.py
+++ trial.py
@@ -1,1 +1,7 @@
-print("Hewllo")
+my_dict = {"Name" : "Paras", "Age" : 2, "Gender" : "Male" }
+for i in my_dict:
+    print(i, my_dict[i])
+for i in my_dict.keys():
+    print(i)
+for i in my_dict.values():
+    print(i)
