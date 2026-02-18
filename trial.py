af80d2f2b37584092749e81b96173be84a3bf84d
SANE_DEVRAJ\PARAS <parasrjpatil111@gmail.com>
2026-02-18 11:30:09 +0530

    continue added

--- trial.py
+++ trial.py
@@ -1,7 +1,14 @@
-my_dict = {"Name" : "Paras", "Age" : 2, "Gender" : "Male" }
-for i in my_dict:
-    print(i, my_dict[i])
-for i in my_dict.keys():
+# my_dict = {"Name" : "Paras", "Age" : 2, "Gender" : "Male" }
+# for i in my_dict:
+#     print(i, my_dict[i])
+# for i in my_dict.keys():
+#     print(i)
+# for i in my_dict.values():
+#     print(i)
+
+for i in range(10):
+    if i == 5:
+        continue
     print(i)
-for i in my_dict.values():
-    print(i)
+
+    
