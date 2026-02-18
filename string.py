e4a38fe1e1ed7d53018d22f28575b62800c53bcc
SANE_DEVRAJ\PARAS <parasrjpatil111@gmail.com>
2026-02-18 10:30:41 +0530

    added if function

---
+++ string.py
@@ -0,0 +1,14 @@
+
+
+my_set = ("Swara", "Paras", "Heramb", "Pavani")
+print(my_set)
+my_set.add("Hobby")
+print(my_set)
+my_set.pop()
+print(my_set)
+
+num1 = 18
+if num1 > 17:
+    status = True
+else: 
+    status = False
